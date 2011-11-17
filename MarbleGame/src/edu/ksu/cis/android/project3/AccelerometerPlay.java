5f133b74fb4752f55463cf8ef18375a88712966a
Brian Sweeney <brians200@gmail.com>
2011-11-17 16:17:15 -0600

    Initial Commit

---
+++ MarbleGame/src/edu/ksu/cis/android/project3/AccelerometerPlay.java
@@ -0,0 +1,13 @@
+package edu.ksu.cis.android.project3;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AccelerometerPlay extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
